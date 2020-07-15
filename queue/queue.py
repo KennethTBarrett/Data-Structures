f0a3295e055a88c36ffeae428e97ed1a24601465
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-07-15 03:20:39 -0500

    Changing location of import.

--- queue/queue.py
+++ queue/queue.py
@@ -1,5 +1,3 @@
-# from singly_linked_list import LinkedList
-
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -38,6 +36,7 @@ Stretch: What if you could only use instances of your Stack class to implement t
 #             return dequeued
 
 ### LAZY WAY FOR MVP - FUNCTIONS. ####
+# from singly_linked_list import LinkedList
 # class Queue:
 #     def __init__(self):
 #         self.size = 0
